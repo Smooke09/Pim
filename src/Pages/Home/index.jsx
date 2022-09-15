65568ce4e2e53785227ca544798258d66ffa462f
Smooke09 <pedrolucas1014zz@gmail.com>
2022-09-14 22:45:20 -0300

    refactoring home page

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -4,14 +4,14 @@ import "./styles.scss";
 
 const Home = () => {
   return (
-    <>
+    <div className="homePage">
       <NavBar />
       <div className="banner-homePage">
         <div className="content-homePage">
           <div className="content-itens-homePage">
             <h2>
-              Assistencia para sua vida e <br />
-              familia de qualquer lugar
+              Assistencia para sua vida e <br />a sua familia de qualquer <br />
+              lugar e qualquer hora
             </h2>
             <button>
               <a href="/login">Fazer login</a>
@@ -19,7 +19,21 @@ const Home = () => {
           </div>
         </div>
       </div>
-    </>
+
+      <div className="content-features-homePage">
+        <div className="content-features-text slide-in-left">
+          <h2>Vamos Começar?</h2>
+          <p>Escolha o seguro que se encaixe melhor com suas necessidades</p>
+        </div>
+        <div className="content-itens-features-homePage">
+          <div className="item-features-homePage">
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
