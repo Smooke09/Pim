34d7cf30c55017c75414de197a8c543b83c930e1
Victor Martinez <victormartinobre@hotmail.com>
2022-10-11 20:04:57 -0300

    feat: :construction: creating-form-simulation

--- src/Pages/Simulation/index.jsx
+++ src/Pages/Simulation/index.jsx
@@ -1,40 +1,127 @@
-import React from "react";
+import React  from "react";
 import "./styles.scss";
 
-const Simulation = () => {
+const simulation = () => {
   return (
-    <div className="content-Simulation-homePage">
-      <div className="content-Simulation-text slide-in-left">
-        <h2>Vamos Começar?</h2>
-        <p>Escolha o seguro que se encaixe melhor com suas necessidades</p>
-      </div>
-      <div className="content-itens-Simulation-homePage">
-        <div className="item-Simulation-homePage">
-          <button>
-            <a href="/Home">VOLTAR</a>
-          </button>
-          <div className="card">
-            <div className="card-content">
-              <div className="card-image">
-                <img src="icon/iconDoenca.svg" alt="img" />
+    <div className="container-simulation-page">
+      <div className="div-button-goBack"></div>
+      <button>
+        <a href="/Home">VOLTAR</a>
+      </button>
+      <div className="simulation-content">
+        <form className="form-simulation">
+          <div className="simulation-form fadeInLeft ">
+            <img src="img/logo2.png" alt="logo" />
+            <div className="imputs-div">
+              <div className="div-imput">
+                <p>Nome:</p>  <input
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <p>Email:</p>  <input
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <p>Telefone:</p>  <input
+                  onChange={(e) => setEmail(e.target.value)}
+                />              
+                <p>Trabalho:</p>  <input
+                  onChange={(e) => setPassword(e.target.value)}
+                /><p>Nome:</p>  <input
+                onChange={(e) => setEmail(e.target.value)}
+                />
+                <p>Email:</p>  <input
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <p>Telefone:</p>  <input
+                  onChange={(e) => setEmail(e.target.value)}
+                />              
+                <p>Trabalho:</p>  <input
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>  
+              <div className="div-imput">       
+                <p>Atividades no trabalho:</p>  <input
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <p>Faixa salarial:</p>  <input
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <p>Mora sozinho?</p>  <input
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <p>Idade:</p>  <input
+                  onChange={(e) => setPassword(e.target.value)}
+                /><p>Atividades no trabalho:</p>  <input
+                onChange={(e) => setEmail(e.target.value)}
+                />
+                <p>Faixa salarial:</p>  <input
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <p>Mora sozinho?</p>  <input
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <p>Idade:</p>  <input
+                  onChange={(e) => setPassword(e.target.value)}
+                />
               </div>
-              <h3>Doencas Graves</h3>
-              <p>
-                Se você vier a faltar por causa de um acidente ou doença, a
-                pessoa que você escolheu como beneficiário receberá o valor
-                contratado.
-              </p>
-              <div className="card-item-button">
-                <button>
-                  <a href="#">Fazer Simulação</a>
-                </button>
+              <div className="div-imput">       
+                <p>Atividades no trabalho:</p>  <input
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <p>Faixa salarial:</p>  <input
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <p>Mora sozinho?</p>  <input
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <p>Idade:</p>  <input
+                  onChange={(e) => setPassword(e.target.value)}
+                /><p>Atividades no trabalho:</p>  <input
+                onChange={(e) => setEmail(e.target.value)}
+                />
+                <p>Faixa salarial:</p>  <input
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <p>Mora sozinho?</p>  <input
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <p>Idade:</p>  <input
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="div-imput">       
+                <p>Atividades no trabalho:</p>  <input
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <p>Faixa salarial:</p>  <input
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <p>Mora sozinho?</p>  <input
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <p>Idade:</p>  <input
+                  onChange={(e) => setPassword(e.target.value)}
+                /><p>Atividades no trabalho:</p>  <input
+                onChange={(e) => setEmail(e.target.value)}
+                />
+                <p>Faixa salarial:</p>  <input
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <p>Mora sozinho?</p>  <input
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <p>Idade:</p>  <input
+                  onChange={(e) => setPassword(e.target.value)}
+                />
               </div>
             </div>
+            <div className="button-simulation">
+              <button>simulation</button>
+            </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
 };
 
-export default Simulation;
+export default simulation;
