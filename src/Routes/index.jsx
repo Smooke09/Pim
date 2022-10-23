b5700fc23cb6342962d71b02c9b9506da87ab221
Victor Martinez <victormartinobre@hotmail.com>
2022-10-23 14:57:28 -0300

    feat: :sparkles: add-profile-page-and-route-profile

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -3,7 +3,8 @@ import { Route, Routes, Link } from "react-router-dom";
 import Home from "../Pages/Home";
 import Login from "../Pages/Login";
 import SingUp from "../Pages/SingUp";
-import Simulation from "../Pages/Simulation"
+import Simulation from "../Pages/Simulation";
+import Profile from "../Pages/Profile";
 import { AuthProvider } from "../context/auth";
 
 const ChangeRoutes = () => {
@@ -14,6 +15,7 @@ const ChangeRoutes = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/singup" element={<SingUp />} />
         <Route path="/simulation" element={<Simulation />} />
+        <Route path="Profile" element={<Profile />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </AuthProvider>
