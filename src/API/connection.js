1c3beb8a46f85c7119b6645b9a89364bb9e27d7f
Pedro Lucas <90714214+Smooke09@users.noreply.github.com>
2022-10-23 12:53:47 -0300

    Update connection.js

--- src/API/connection.js
+++ src/API/connection.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const connection = axios.create({
   // Production
-  baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "https://pim-api.herokuapp.com/api"
+//   baseURL: import.meta.env.VITE_API_URL,
   // dev
   // baseURL: "http://localhost:3333/api",
   headers: {
