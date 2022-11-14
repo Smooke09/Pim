4b8cdda2cea8f8ad45f94133f852a65dee428482
Smooke09 <pedrolucas1014zz@gmail.com>
2022-11-13 21:08:23 -0300

    chore: add vercel.json

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes, Link } from "react-router-dom";
+
 import Home from "../Pages/Home";
 import Login from "../Pages/Login";
 import SingUp from "../Pages/SingUpProfile";
